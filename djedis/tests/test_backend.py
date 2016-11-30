2452b85a66483cae8534b7447ed8c7de4041053a
Yura Revutskiy <dr.bleedjent@gmail.com>
2016-11-30 17:35:41 +0200

    Work on tests

--- djedis/tests/test_backend.py
+++ djedis/tests/test_backend.py
@@ -1,18 +1,2 @@
 from unittest import TestCase
-from django.conf import settings
-from django.core.cache import cache
-settings.configure()
-
 from djedis.settings import DEFAULT_CACHES
-from djedis.backend import RedisCache
-
-settings['CACHES'] = DEFAULT_CACHES
-
-
-class RedisDjangoBackendTestCase(TestCase):
-
-    def setUp(self):
-        self.cache = RedisCache()
-
-    def test__set(self):
-        pass
--- djedis/tests/tests_utils.py
+++ djedis/tests/tests_utils.py
@@ -12,18 +12,18 @@ CACHES.update(DEFAULT_CACHES)
 class UtilsTestCase(TestCase):
 
     def test__make_key(self):
-        fine_key = md5('foo:1').hexdigest()
+        fine_key = 'foo:1'
         self.assertIsInstance(make_key('foo', 1), str)
         self.assertEqual(make_key('foo', 1), fine_key)
         self.assertIsInstance(make_key(u'вот'), str)
         self.assertIsInstance(make_key(u'вот', 2), str)
 
-    def test__chose_index(self):
-        key1 = make_key('foo')
-        key2 = make_key('bar')
-        pool = RedisPoolFactory()
-        _pool_keys = pool.keys()
-        index1 = chose_index(key1, _pool_keys)
-        index2 = chose_index(key2, _pool_keys)
-        self.assertEqual(chose_index(key1, _pool_keys), index1)
-        self.assertEqual(chose_index(key2, _pool_keys), index2)
+    # def test__chose_index(self):
+    #     key1 = make_key('foo')
+    #     key2 = make_key('bar')
+    #     pool = RedisPoolFactory()
+    #     _pool_keys = pool.keys()
+    #     index1 = chose_index(key1, _pool_keys)
+    #     index2 = chose_index(key2, _pool_keys)
+    #     self.assertEqual(chose_index(key1, _pool_keys), index1)
+    #     self.assertEqual(chose_index(key2, _pool_keys), index2)
