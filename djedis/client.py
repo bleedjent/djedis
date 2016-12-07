dd4331ccce0ed532ebdd59495bd1cf7122e340ea
Yura Revutskiy <dr.bleedjent@gmail.com>
2016-12-07 10:15:37 +0200

    Use scan instead keys

--- djedis/client.py
+++ djedis/client.py
@@ -116,7 +116,7 @@ class ShardClient(object):
     def keys(self, pattern='*'):
         _keys = set()
         for client in self._pool.values():
-            _keys.update(client.keys(pattern))
+            _keys.update(client.scan(match=pattern))
         return _keys
 
     def delete_pattern(self, pattern):
